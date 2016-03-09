f0720e22b014024eb2f876aa1ce7b3d10d6b332b
7six <maste2seven@gmail.com>
2016-03-09 11:35:38 +0700

    check id card finish

--- app/src/main/java/com/example/rit/bikeforrent/signUp.java
+++ app/src/main/java/com/example/rit/bikeforrent/signUp.java
@@ -2,17 +2,65 @@ package com.example.rit.bikeforrent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class signUp extends AppCompatActivity {
 
-    public signUp(MainActivity mainActivity, Class<signUp> signUpClass) {
+    private EditText idcardEditText, passEditText;
+    private String idcardString, passString;
 
 
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //bindwiget
+        bindwiget();
+
+    } //method
+
+
+    private void bindwiget() {
+
+
+        idcardEditText = (EditText) findViewById(R.id.editText);
+        passEditText = (EditText) findViewById(R.id.editText2);
+
+
+        //chexkspace
+
+        if (idcardEditText.equals("") || passEditText.equals("")) {
+            //have space
+            myToast("กรอกมูลไม่ครบ");
+        } else {
+            //no have space
+            checkidcard();
+
+        }
     }
-}
+
+    private void checkidcard() {
+
+        if (idcardString.length()==13) {
+            //idture
+        } else {
+            myToast("รหัสบัตรผิด");
+        }
+
+    }//checkID
+
+    private void myToast(String strtoast) {
+        Toast.makeText(signUp.this, strtoast, Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void onClickSignUp(View view) {
+        idcardString = idcardEditText.getText().toString().trim();
+        passString = passEditText.getText().toString().trim();
+    }
+
+}//main class
