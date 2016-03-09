ed6e24744d0caed76fc62f95cb7d9f41724d3d4f
7six <maste2seven@gmail.com>
2016-03-09 10:31:20 +0700

    Sign Up

--- app/src/main/java/com/example/rit/bikeforrent/MainActivity.java
+++ app/src/main/java/com/example/rit/bikeforrent/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.rit.bikeforrent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }//main method
+    public void onclickSignUp (View view) {
+        startActivity(new signUp(MainActivity.this, signUp.class));
     }
-}
+
+    private void startActivity(signUp signUp) {
+
+
+    }
+}//main class
