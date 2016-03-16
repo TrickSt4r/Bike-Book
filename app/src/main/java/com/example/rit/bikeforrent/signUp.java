820181a63f3bbfe8ae6abe9e8ee2ab4d51bcb08d
7six <maste2seven@gmail.com>
2016-03-16 10:42:11 +0700

    Confirm Dialog Finish

--- app/src/main/java/com/example/rit/bikeforrent/signUp.java
+++ app/src/main/java/com/example/rit/bikeforrent/signUp.java
@@ -1,5 +1,7 @@
 package com.example.rit.bikeforrent;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,9 @@ public class signUp extends AppCompatActivity {
     private EditText idcardEditText, passEditText;
     private String idcardString, passString;
 
+    public signUp(MainActivity mainActivity, Class<signUp> signUpClass) {
+
+    }
 
 
     @Override
@@ -49,11 +54,42 @@ public class signUp extends AppCompatActivity {
 
         if (idcardString.length()==13) {
             //idture
+            confirmData(idcardString, passString);
         } else {
             myToast("รหัสบัตรไม่ถูกต้อง");
         }
 
     }//checkID
+
+    private void confirmData(String idcardString, String passString) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //wati photo builder.setIcon(R.drawable.);
+        builder.setTitle("โปรดตรวจสอบข้อมูล");
+        builder.setMessage("รหัสบัตรประชาชน = " + idcardString + "\n" + "password = " + passString);
+        builder.setCancelable(false);
+        builder.setPositiveButton("confrim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                updateDataToSever();
+
+            }
+        });
+        builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }//onclick
+        });
+        builder.show();
+
+    }//confirmData
+
+    private void updateDataToSever() {
+
+
+    }
 
     private void myToast(String strtoast) {
         Toast.makeText(signUp.this, strtoast, Toast.LENGTH_SHORT).show();
