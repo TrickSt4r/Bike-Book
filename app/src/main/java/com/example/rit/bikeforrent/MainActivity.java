bc34b8bc6ff449fd860fc352014dff7fd4d514d5
7six <maste2seven@gmail.com>
2016-03-16 12:05:50 +0700

    Finish 16/3/2559

--- app/src/main/java/com/example/rit/bikeforrent/MainActivity.java
+++ app/src/main/java/com/example/rit/bikeforrent/MainActivity.java
@@ -1,16 +1,56 @@
 package com.example.rit.bikeforrent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //exelpit
+    private EditText idCardEditText, passwordEditText;
+    private String idString, passwordString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //bindwiget
+
+        bindwiget();
+
+
     }//main method
+
+    private void bindwiget() {
+        idCardEditText = (EditText) findViewById(R.id.editText3);
+        passwordEditText = (EditText) findViewById(R.id.editText4);
+
+
+    }
+
+    public void clicksignMain(View view) {
+
+        idString = idCardEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //check
+        if (idString.equals("")||passwordString.equals("")) {
+
+            myAlert("กรุณากรอกข้อมูลให้ครบ");
+        }
+
+    }//clicksignMain
+
+    private void myAlert(String strAlert) {
+
+        Toast.makeText(MainActivity.this, strAlert, Toast.LENGTH_SHORT).show();
+    }
+
     public void onclickSignUp (View view) {
         startActivity(new signUp(MainActivity.this, signUp.class));
     }
